01d44e0699d4205ddc409dfe9c0362cb655107bd
jdrescher <jdrescher@a1cee49e-58f3-4468-91e7-9a2fb1bbd407>
2020-10-02 20:53:36 +0000

    Begin to add an additional seed widget example to see if I can understand how the rendering is broken in software rendering mode.
    git-svn-id: http://192.168.1.246:8084/svn/medVtkInria/trunk@49 a1cee49e-58f3-4468-91e7-9a2fb1bbd407

---
+++ Development/QtFourPaneImageViewer4/src/QtVTKRenderWindowsApp.cxx
@@ -0,0 +1,30 @@
+#include <QApplication>
+#include <QSurfaceFormat>
+#include <QMessageBox>
+
+#include "QVTKOpenGLWidget.h"
+#include "QtVTKRenderWindows.h"
+
+int main( int argc, char** argv )
+{
+	if (argc > 1) {
+
+		// needed to ensure appropriate OpenGL context is created for VTK rendering.
+		QSurfaceFormat::setDefaultFormat(QVTKOpenGLWidget::defaultFormat());
+
+		// QT Stuff
+		QApplication app(argc, argv);
+
+		QtVTKRenderWindows myQtVTKRenderWindows(argc, argv);
+		myQtVTKRenderWindows.show();
+
+		return app.exec();
+	}
+	else {
+		QMessageBox::warning(nullptr, "The application will close.", 
+			"This program requires 1 command line argument which is the path of a folder containing a DICOM series.");
+
+	}
+
+	return -1;
+}
